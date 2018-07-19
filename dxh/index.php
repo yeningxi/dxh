b44763411853e290dc70e75ec1f364e6b821798f
ittiro <hmy19901008@163.com>
2018-07-19 22:13:05 +0800

    secend-company

--- dxh/index.php
+++ dxh/index.php
@@ -10,9 +10,9 @@ class Active{
         @判断是否登录
          */
         if (isset($_SESSION['openid']) && $_SESSION['openid']){
-            return ['code'=>200,'msg'=>'用户已经登录'];
+            return json_encode(['code'=>200,'msg'=>'用户已经登录']);
         }else{
-            return ['code'=>302,'msg'=>'请登录'];
+            return json_encode(['code'=>302,'msg'=>'请登录']);
         }
     }
 
@@ -22,7 +22,7 @@ class Active{
     public function auth()
     {
         $_SESSION['openid'] = '123456789';
-        return ['code'=>200,'msg'=>'登录成功'];
+        return json_encode(['code'=>200,'msg'=>'登录成功']);
     }
 
     /**
@@ -32,11 +32,15 @@ class Active{
     {
         $pdo = DB::getIntense();
         $res = $pdo->query('select * from active_buyer where openid="'.$_SESSION['openid'].'" limit 1');
+        if (!$res) {
+            # code...
+            return json_encode(['code'=>301,'msg'=>'暂无订单']);
+        }
         $row = $res->fetchAll(PDO::FETCH_ASSOC);
         if (!empty($row)){
-            return ['code'=>200,'msg'=>'ok','data'=>$row];
+            return json_encode(['code'=>200,'msg'=>'ok','data'=>$row]);
         }
-        return ['code'=>301,'msg'=>'暂无订单'];
+        
     }
 
     /**
@@ -50,13 +54,13 @@ class Active{
         $phonenum = $_POST['phonenum'];
         $address  = $_POST['address'];
         if (!isset($_SESSION['openid']) || !$_SESSION['openid']){
-            return ['code'=>301,'msg'=>'用户未登录'];
+            return json_encode(['code'=>301,'msg'=>'用户未登录']);
         }
-        if (!preg_match('/^\d{11}$/')){
-            return ['code'=>400,'msg'=>'手机号格式不正确'];
+        if (!preg_match('/^\d{11}$/',$phonenum)){
+            return json_encode(['code'=>400,'msg'=>'手机号格式不正确']);
         }
         if (!trim($address)){
-            return ['code'=>400,'msg'=>'收货地址必须填写'];
+            return json_encode(['code'=>400,'msg'=>'收货地址必须填写']);
         }
         if ($num!=1){
             $num = 1;
@@ -66,35 +70,74 @@ class Active{
 
         $repertory = $pdo->query('select repertory,starttime,endtime from active_goods where id=1 for update');
         $repertory_num = $repertory->fetchAll(PDO::FETCH_ASSOC);
+        if (empty($repertory_num)) {
+            # code...
+            return json_encode(['code'=>400,'msg'=>'没有活动商品']);
+        }
         if ($repertory_num[0]['repertory']<=0){
-            return ['code'=>400,'msg'=>'已售罄'];
+            return json_encode(['code'=>400,'msg'=>'已售罄']);
         }
         if ($repertory_num[0]['starttime']<time()){
-            return ['code'=>400,'msg'=>'活动还未开始'];
+            return json_encode(['code'=>400,'msg'=>'活动还未开始']);
         }
         if ($repertory_num[0]['endtime']>time()){
-            return ['code'=>400,'msg'=>'活动已结束'];
+            return json_encode(['code'=>400,'msg'=>'活动已结束']);
         }
-        $buy_sql = 'insert into active_buyer(openid,nick,num,phonenum,address) values (:openid,:nick,:num,:phonenum,:address)';
+
+        $freeorder = $pdo->query('select count(*) as count from active_buyer');
+        $buyuser = $freeorder->fetchAll(PDO::FETCH_ASSOC);
+        if ($buyuser['0']['count']<=100) {
+            # code...
+            $status=1;
+        }else{
+            $status=0;
+        }
+        $buy_sql = 'insert into active_buyer(openid,nick,num,phonenum,address,status) values (:openid,:nick,:num,:phonenum,:address,:status)';
         $query = $pdo->prepare($buy_sql);
         $result = $query->execute(array(
             ':openid'=>$openid,
             ':nick'=>$nick,
             ':num'=>$num,
             ':phonenum'=>$phonenum,
-            ':address'=>$address
-
+            ':address'=>$address,
+            ':status'=>$status
         ));
         if (!$result){
             $pdo->rollback();
-            return ['code'=>400,'msg'=>'购买失败'];
+            return json_encode(['code'=>400,'msg'=>'购买失败']);
         }
         $goods_sql = 'update active_goods set num=num-1 where id=1';
         $res = $pdo->exec($goods_sql);
         if ($res<=0){
             $pdo->rollback();
-            return ['code'=>400,'msg'=>'更改商品表失败'];
+            return json_encode(['code'=>400,'msg'=>'更改商品表失败']);
         }
+        $pdo->commit();
+        return json_encode(['code'=>200,'msg'=>'下单成功，请尽快支付']);
+    }
+
+    /**
+     @支付
+    */
+    public function pay()
+    {
+
+    }
+
+    /**
+     支付回调   
+    */
+    public function payback
+    {   
+        $pdo = DB::getIntense();
+    
+        //失败
+        //TODO
+        //成功
+
+        $sql = 'update active_buyer set status=1 where openid="'.$_SESSION['openid'].'"';
+        $res = $pdo->exec($sql);
+        return json_encode(['code'=>200,'msg'=>'支付成功']);
     }
 }
 
